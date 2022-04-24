99a356939943e17b9e2d486763b775f672469d60
Alexander Savinykh <asavinykh@nbrx.de>
2022-04-24 17:20:21 +0200

    Added function get_median

--- vector/src/main.rs
+++ vector/src/main.rs
@@ -1,9 +1,23 @@
 fn main() {
-    let mut vector = vec![24, 4, 2022, 16, 50, 4, 50];
-    println!("{:?}", vector);
-    vector.sort();
-    println!("{:?}", vector);
-    let median_index = vector.len() / 2;
-    let median = vector.get(median_index);
-    println!("Median {:?} at index {}", median, median_index);
+    let vector = vec![24, 4, 2022, 16, 50, 4, 50];
+    let median = get_median(&vector);
+    println!("Median {:?}", median);
+}
+
+
+#[derive(Debug)]
+struct Median {
+    index: usize,
+    value: isize,
+}
+
+fn get_median(vector: &Vec<isize>) -> Option<Median> {
+    let mut sorted_vector = vector.clone();
+    sorted_vector.sort();
+    let index = sorted_vector.len() / 2;
+    let median = sorted_vector.get(index);
+    match median {
+        Some(value) => Some(Median{index, value: *value}),
+        None => None
+    }
 }
