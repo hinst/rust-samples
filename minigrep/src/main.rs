98b36efab078947751b029cd668f5125ce7d7b49
Alexander Savinykh <asavinykh@nbrx.de>
2022-06-06 15:32:52 +0200

    Unwrap config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::from_args(&args);
+    let config = Config::from_args(&args).unwrap_or_else(|err| {
+        println!("Unable to read arguments: {}", err);
+        process::exit(1);
+    });
     println!("Search for {} in file {}", config.query, config.file_name);
     let file_content = fs::read_to_string(config.file_name.as_str())
         .expect(format!("Read file {}", config.file_name).as_str());
@@ -16,15 +20,16 @@ struct Config {
 }
 
 impl Config {
-    fn from_args(args: &[String]) -> Config {
+    fn from_args(args: &[String]) -> Result<Config, String> {
         if args.len() < 3 {
-            panic!("Not enough arguments")
+            return Err(String::from("Not enough arguments"));
         }
         let query = args[1].clone();
         let file_name = args[2].clone();
-        return Config {
+        let config = Config {
             query,
             file_name,
         };
+        return Ok(config);
     }
 }
