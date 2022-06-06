6b667a7987e27ca8a730cfeb480b0ddd7b8c25ba
Alexander Savinykh <asavinykh@nbrx.de>
2022-06-06 19:24:28 +0200

    Adding struct Cacher

--- le_closure/src/main.rs
+++ le_closure/src/main.rs
@@ -3,23 +3,45 @@ use std::time::Duration;
 
 fn main() {
     println!("STARTING...");
-    generate_workout(25, 4);
+    generate_workout(24, 4);
+}
+
+struct Cacher<T> where T: Fn(u32) -> u32 {
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T> where T: Fn(u32) -> u32 {
+    fn new(calculation: T) -> Cacher<T> {
+        return Cacher { calculation, value: None }
+    }
+
+    fn get_value(&mut self, arg: u32) -> u32{
+        match self.value {
+            Some(value) => value,
+            None => {
+                let value = (self.calculation)(arg);
+                self.value = Some(value);
+                return value;
+            }
+        }
+    }
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_closure = |num| {
+    let mut cacher = Cacher::new(|num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
     if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_closure(intensity));
-        println!("Next, do {} situps!", expensive_closure(intensity));
+        println!("Today, do {} pushups!", cacher.get_value(intensity));
+        println!("Next, do {} situps!", cacher.get_value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated");
         } else {
-            println!("Today, run for {} minutes", expensive_closure(intensity));
+            println!("Today, run for {} minutes", cacher.get_value(intensity));
         }
     }
 }
