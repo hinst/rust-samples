b018bad9215d1abf58a77f49333038217a2da0eb
Alexander Savinykh <asavinykh@nbrx.de>
2022-06-06 17:28:35 +0200

    Adding sub-project: le_closure

---
+++ le_closure/src/main.rs
@@ -0,0 +1,25 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    println!("STARTING...");
+    generate_workout(25, 4);
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_closure = |num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expensive_closure(intensity));
+        println!("Next, do {} situps!", expensive_closure(intensity));
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated");
+        } else {
+            println!("Today, run for {} minutes", expensive_closure(intensity));
+        }
+    }
+}
