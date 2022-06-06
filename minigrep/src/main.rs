a33bf1c22161d1b7be468f569ddaf8ed1d4556be
Alexander Savinykh <asavinykh@nbrx.de>
2022-06-06 15:16:28 +0200

    Parse config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,9 +3,23 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_name = &args[2];
-    println!("Search for {} in file {}", query, file_name);
-    let file_content = fs::read_to_string(file_name).expect(format!("Read file {}", file_name).as_str());
+    let config = parse_arguments(&args);
+    println!("Search for {} in file {}", config.query, config.file_name);
+    let file_content = fs::read_to_string(config.file_name.as_str())
+        .expect(format!("Read file {}", config.file_name).as_str());
     println!("{}", file_content);
+}
+
+struct Config {
+    query: String,
+    file_name: String,
+}
+
+fn parse_arguments(args: &[String]) -> Config {
+    let query = String::from(&args[1]);
+    let file_name = String::from(&args[2]);
+    return Config {
+        query,
+        file_name,
+    };
 }
