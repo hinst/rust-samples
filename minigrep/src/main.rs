cdb589f64141e2a7438e7269ef1271f170fb6eaf
Alexander Savinykh <asavinykh@nbrx.de>
2022-06-06 15:23:31 +0200

    Config::from args

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_arguments(&args);
+    let config = Config::from_args(&args);
     println!("Search for {} in file {}", config.query, config.file_name);
     let file_content = fs::read_to_string(config.file_name.as_str())
         .expect(format!("Read file {}", config.file_name).as_str());
@@ -15,11 +15,16 @@ struct Config {
     file_name: String,
 }
 
-fn parse_arguments(args: &[String]) -> Config {
-    let query = String::from(&args[1]);
-    let file_name = String::from(&args[2]);
-    return Config {
-        query,
-        file_name,
-    };
+impl Config {
+    fn from_args(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("Not enough arguments")
+        }
+        let query = args[1].clone();
+        let file_name = args[2].clone();
+        return Config {
+            query,
+            file_name,
+        };
+    }
 }
