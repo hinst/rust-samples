9147da4d1179c8445ebebc102ccec5beb9f5bb00
Alexander Savinykh <asavinykh@nbrx.de>
2022-06-06 15:11:27 +0200

    Read arguments, read file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,11 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let query = &args[1];
+    let file_name = &args[2];
+    println!("Search for {} in file {}", query, file_name);
+    let file_content = fs::read_to_string(file_name).expect(format!("Read file {}", file_name).as_str());
+    println!("{}", file_content);
 }
