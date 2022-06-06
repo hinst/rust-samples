29340528646df73a8b48c79b77c33ee1324066e1
Alexander Savinykh <asavinykh@nbrx.de>
2022-06-06 12:45:50 +0200

    Use generic for IndexedValue

--- vector/src/main.rs
+++ vector/src/main.rs
@@ -9,42 +9,42 @@ fn main() {
 }
 
 #[derive(Debug)]
-struct IndexedIsize {
+struct IndexedValue<T> {
     index: usize,
-    value: isize,
+    value: T,
 }
 
-fn get_median(vector: &Vec<isize>) -> Option<IndexedIsize> {
+fn get_median<T: Ord + Copy>(vector: &Vec<T>) -> Option<IndexedValue<T>> {
     let mut sorted_vector = vector.clone();
     sorted_vector.sort();
     let index = sorted_vector.len() / 2;
     let median = sorted_vector.get(index);
     match median {
-        Some(value) => Some(IndexedIsize{index, value: *value}),
+        Some(value) => Some(IndexedValue{index, value: *value}),
         None => None
     }
 }
 
 #[derive(Debug)]
-struct CountedIsize {
+struct CountedValue<T> {
     count: usize,
-    value: isize,
+    value: T,
 }
 
 /** Mode is a value which appears in the array more often then all other values */
-fn get_mode(vector: &Vec<isize>) -> Option<CountedIsize> {
-    let mut occurrences: HashMap<isize, usize> = HashMap::new();
+fn get_mode<T: Ord + Copy + std::hash::Hash>(vector: &Vec<T>) -> Option<CountedValue<T>> {
+    let mut occurrences: HashMap<T, usize> = HashMap::new();
     for value in vector {
         let count = occurrences.entry(*value).or_insert(0);
         *count += 1;
     };
-    let mut max_value: Option<CountedIsize> = None;
+    let mut max_value: Option<CountedValue<T>> = None;
     for (value, count) in occurrences {
         match &max_value {
-            None => max_value = Some(CountedIsize{ count, value }),
+            None => max_value = Some(CountedValue{ count, value }),
             Some(current_max_value) => {
                 if current_max_value.count < count {
-                    max_value = Some(CountedIsize { count, value });
+                    max_value = Some(CountedValue { count, value });
                 }
             }
         }
